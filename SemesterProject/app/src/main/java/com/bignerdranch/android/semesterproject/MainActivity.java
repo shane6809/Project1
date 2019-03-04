5514735eb02109afd088d574148fa48fcc7ab949
Shane <shane6809@hotmail.com>
2019-03-04 15:53:47 -0600

    First layout connected, added logs, added some design to game_select

--- SemesterProject/app/src/main/java/com/bignerdranch/android/semesterproject/MainActivity.java
+++ SemesterProject/app/src/main/java/com/bignerdranch/android/semesterproject/MainActivity.java
@@ -1,13 +1,77 @@
 package com.bignerdranch.android.semesterproject;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="Memory Match";
+    private Button mStartButton;
+    private Button mHighScoresButton;
+    private Button mSettingsButton;
+    private Button mHelpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle)called");
         setContentView(R.layout.activity_main);
+
+        mStartButton=(Button)findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start button
+                Intent intent = new Intent(MainActivity.this, game_select.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+
+
+
+
+
 }
+
+
+
+
