b084e784c8e258fe9f8f0c103a7b9b87e94466e5
Shane <shane6809@hotmail.com>
2019-03-12 14:57:22 -0500

    Test 3, working template for sensors

--- SensorPractice/app/src/main/java/com/bignerdranch/android/sensorpractice/MainActivity.java
+++ SensorPractice/app/src/main/java/com/bignerdranch/android/sensorpractice/MainActivity.java
@@ -1,14 +1,73 @@
 package com.bignerdranch.android.sensorpractice;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    /* Test 2 after File Sync */
+    private SensorManager sensorManager;
+    private Sensor gyroscopeSensor;
+    private SensorEventListener gyroscopeSensorListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        gyroscopeSensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if (gyroscopeSensor == null) {
+            Toast.makeText(this, "Gyro Test", Toast.LENGTH_SHORT).show();
+            finish();
+
+        }
+
+        // Create a listener
+        gyroscopeSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                // More code goes here
+                if (sensorEvent.values[2] > 0.5f) { // anticlockwise
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                } else if (sensorEvent.values[2] < -0.5f) { // clockwise
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
     }
+
+    // Register the listener
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(gyroscopeSensorListener,
+                gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(gyroscopeSensorListener);
+    }
+
 }
-/* Testing after File Sync */
+
