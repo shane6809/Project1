345129a0cb4daf7e4109482696e6e00115093215
Shane <shane6809@hotmail.com>
2019-03-31 12:22:51 -0500

    Added game_settings button for user name, added user_name java class, added layout reverted to basic activity, first setup of activity_user_name.xml

--- SemesterProject/app/src/main/java/com/bignerdranch/android/semesterproject/user_name.java
+++ SemesterProject/app/src/main/java/com/bignerdranch/android/semesterproject/user_name.java
@@ -1,6 +1,28 @@
 package com.bignerdranch.android.semesterproject;
 
-public class user_name {
-    
-    
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class user_name extends AppCompatActivity {
+
+    private Button mBackButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_name);
+
+        mBackButton=(Button)findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(user_name.this, game_settings.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
+
