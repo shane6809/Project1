caf18d27d7f9a8fcc7d2c11be2833c2a23174ad0
Shane <shane6809@hotmail.com>
2019-04-14 09:52:29 -0500

    added some comments, added sensor, sensor tested and working for home page.

--- SemesterProject/app/src/main/java/com/bignerdranch/android/semesterproject/MainActivity.java
+++ SemesterProject/app/src/main/java/com/bignerdranch/android/semesterproject/MainActivity.java
@@ -3,6 +3,11 @@ package com.bignerdranch.android.semesterproject;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.nfc.Tag;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -15,15 +20,22 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG="Memory Match";
+
+    /*button variables */
     private Button mStartButton;
     private Button mHighScoresButton;
     private Button mSettingsButton;
     private Button mHelpButton;
+
+    /*Storage variables*/
     boolean mExternalStorageAvailable = false;
     boolean mExternalStorageWriteable = false;
 
 
-
+    /*light sensor variables*/
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lightSensorListener;
 
 
 
@@ -33,7 +45,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle)called");
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
+        lightSensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        //message for hardware not having the correct sensor
+        if (lightSensor == null) {
+            Toast.makeText(this, "You do not have the necessary sensor",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+
+        }
+
+        // Create listener
+        lightSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float value = sensorEvent.values[0];
+
+                // Code for screen change based on light
+                if (sensorEvent.values[0] > 30000f) { // Brighter color for easy reading in high light
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                    Toast.makeText(MainActivity.this, "Luminosity" + value, Toast.LENGTH_SHORT).show();
+
+                }
+
+                if (sensorEvent.values[0] > 22001f && sensorEvent.values[0] < 29999) { // Brighter color for easy reading in medium-high light
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                    Toast.makeText(MainActivity.this, "Luminosity" + value, Toast.LENGTH_SHORT).show();
+
+                }
+
+                if (sensorEvent.values[0] > 17001f && sensorEvent.values[0] < 22000) { // Brighter color for easy reading in medium light
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                    Toast.makeText(MainActivity.this, "Luminosity" + value, Toast.LENGTH_SHORT).show();
+
+                }
+
+                if (sensorEvent.values[0] > 12001f && sensorEvent.values[0] < 17000) { // Brighter color for easy reading in medium light
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                    Toast.makeText(MainActivity.this, "Luminosity" + value, Toast.LENGTH_SHORT).show();
+
+                } else if (sensorEvent.values[0] < 12000f) { // Night mode for low light readability using DarkViolet
+                    getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.DarkViolet));
+                    Toast.makeText(MainActivity.this, "Luminosity" + value, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
 
         //check external storage state
         String state = Environment.getExternalStorageState();
@@ -91,6 +154,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
     }
 
     @Override
@@ -105,12 +171,15 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume() called");
+        sensorManager.registerListener(lightSensorListener,
+                lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public void onPause() {
         super.onPause();
         Log.d(TAG, "onPause() called");
+        sensorManager.unregisterListener(lightSensorListener);
     }
 
     @Override
