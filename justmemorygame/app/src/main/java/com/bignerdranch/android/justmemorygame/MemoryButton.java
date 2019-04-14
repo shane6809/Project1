da9c732035f2d33e39b9e9a8a2ae533e9b376150
Shane <shane6809@hotmail.com>
2019-04-14 17:14:35 -0500

    memorybutton code finished, need to go through and do some stuff to make it more compatible with a newer version of sdk, but no red squigglies.

---
+++ justmemorygame/app/src/main/java/com/bignerdranch/android/justmemorygame/MemoryButton.java
@@ -0,0 +1,90 @@
+package com.bignerdranch.android.justmemorygame;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatDrawableManager;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import static java.lang.reflect.Array.get;
+
+public class MemoryButton extends AppCompatButton {
+
+    protected int row;
+    protected int column;
+    protected int frontDrawableId;
+
+    protected boolean isFlipped = false;
+    protected boolean isMatched = false;
+
+    protected Drawable front;
+    protected Drawable back;
+
+
+    @SuppressLint("RestrictedApi")
+    public MemoryButton(Context context, int r, int c,
+                        int frontImageDrawableId){
+
+
+
+        super(context);
+
+        row = r;
+        column = c;
+        frontDrawableId = frontImageDrawableId;
+
+
+
+        front = AppCompatDrawableManager.get().getDrawable(context,frontImageDrawableId);
+        back = AppCompatDrawableManager.get().getDrawable(context,R.drawable.question);
+
+        setBackground(back);
+
+
+        GridLayout.LayoutParams tempParams = new GridLayout.LayoutParams(GridLayout.spec(r), GridLayout.spec(c));
+
+        tempParams.width =(int) getResources().getDisplayMetrics().density *50;
+        tempParams.height =(int) getResources().getDisplayMetrics().density *50;
+
+
+        setLayoutParams(tempParams);
+
+    }
+
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
+
+    public int getFrontDrawableId() {
+        return frontDrawableId;
+    }
+
+    public void flip(){
+        if(isMatched)
+            return;
+
+        if(isFlipped){
+            setBackground(back);
+            isFlipped = false;
+
+
+        }
+
+        else{
+            setBackground(front);
+            isFlipped = true;
+
+        }
+
+
+    }
+
+}
