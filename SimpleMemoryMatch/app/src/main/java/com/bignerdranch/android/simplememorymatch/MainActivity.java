17ee0efa6fbc325504cd2d9d7526b14088fe7bc7
Shane <shane6809@hotmail.com>
2019-04-16 16:48:33 -0500

    Completed flip game, need to get images to scale properly, need to make it stop from being flipped again. cleaned up code.

--- SimpleMemoryMatch/app/src/main/java/com/bignerdranch/android/simplememorymatch/MainActivity.java
+++ SimpleMemoryMatch/app/src/main/java/com/bignerdranch/android/simplememorymatch/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridView = findViewById(R.id.gridView);
+        final GridView gridView = findViewById(R.id.gridView);
         ImageAdapter imageAdapter = new ImageAdapter(this);
         gridView.setAdapter(imageAdapter);
 
@@ -63,7 +63,9 @@ public class MainActivity extends AppCompatActivity {
 
                         countPair++;
 
-                        if(countPair == 0){
+                        if(countPair==0){
+
+                            gridView.setOnItemClickListener(null);
 
                             Toast.makeText(getApplicationContext(),
                                     "You have Won",Toast.LENGTH_SHORT).show();
